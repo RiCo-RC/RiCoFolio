2649f7442fa966d771738e2a6e243400657ca640
RiCo-RC <christopher.rico.rieffel@gmail.com>
2026-03-23 10:37:14 +0100

    edit layout 3

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,40 +1,31 @@
-import { Metadata } from "next";
-import { ReactNode } from "react";
-import { SUPPORTED_LANGS, LangSync } from "@src/lib";
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
-export const generateStaticParams = async () =>
-	SUPPORTED_LANGS.map((lang) => ({ lang }));
+import { SUPPORTED_LANGS, LangSync } from '@src/lib';
 
-export const generateMetadata = async ({
-	params,
-}: {
-	params: { lang: string };
-}): Promise<Metadata> => {
-	const { lang } = params;
-	return {
-		alternates: {
-			languages: SUPPORTED_LANGS.reduce(
-				(acc, cur) => {
-					acc[cur] = `/${cur}`;
-					return acc;
-				},
-				{} as Record<string, string>,
-			),
-		},
-		other: {
-			"content-language": lang,
-		},
-	};
+export const generateStaticParams = async () => (
+  SUPPORTED_LANGS.map((lang) => ({ lang }))
+);
+
+export const generateMetadata = async ({ params }: { params: Promise<{ lang: string }> }): Promise<Metadata> => {
+  const { lang } = await params; 
+  
+  return {
+    alternates: {
+      languages: SUPPORTED_LANGS.reduce((acc, cur) => {
+        acc[cur] = `/${cur}`;
+        return acc;
+      }, {} as Record<string, string>),
+    },
+    other: {
+      'content-language': lang,
+    },
+  };
 };
 
-const LangLayout = ({
-	children,
-	params,
-}: {
-	children: ReactNode;
-	params: { lang: string };
-}) => {
-	return <LangSync lang={params.lang}>{children}</LangSync>;
+const LangLayout = async ({ children, params }: { children: ReactNode; params: Promise<{ lang: string }> }) => {
+  const { lang } = await params;
+  return <LangSync lang={lang}>{children}</LangSync>;
 };
 
 export default LangLayout;
