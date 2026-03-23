46dccc31aaea94329b81d044e37e1c6b0df832e1
RiCo-RC <christopher.rico.rieffel@gmail.com>
2026-03-23 10:21:20 +0100

    edit layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,5 @@
-import { Metadata } from 'next';
-import { ReactNode } from 'react';
-
+import { Metadata } from 'next'; 
+import { ReactNode, Suspense } from 'react';
 import { SUPPORTED_LANGS, LangSync } from '@src/lib';
 
 export const generateStaticParams = async () => (
@@ -22,9 +21,17 @@ export const generateMetadata = async ({ params }: { params: { lang: string } })
   };
 };
 
-const LangLayout = async ({ children, params }: { children: ReactNode; params: { lang: string } }) => {
-  const { lang } = await params;
-  return <LangSync lang={lang}>{children}</LangSync>;
+const LangLayout = ({ children, params }: { children: ReactNode; params: { lang: string } }) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AsyncLangSync lang={params.lang}>{children}</AsyncLangSync>
+    </Suspense>
+  );
 };
 
 export default LangLayout;
+
+const AsyncLangSync = async ({ lang, children }: { lang: string; children: ReactNode }) => {
+  const data = await lang; 
+  return <LangSync lang={data}>{children}</LangSync>;
+};
